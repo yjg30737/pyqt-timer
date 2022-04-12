86abb71d5d67fcc1207dd111d7ade7a646aa12fe
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-12 18:35:31 +0900

    Add new icon to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'pyqt_timer.ico': ['play.svg', 'pause.svg', 'stop.svg', 'settings.svg']},
+    package_data={'pyqt_timer.ico': ['pause.svg', 'play.svg', 'refresh.svg', 'settings.svg', 'stop.svg']},
     description='Simple timer made out of PyQt',
     url='https://github.com/yjg30737/pyqt-timer.git',
     install_requires=[
